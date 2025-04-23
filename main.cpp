fcba3fbd4f6f62daac61dfcc792aa4731443f600
Alon <alon@Rosss-Macbook-13-Alon.local>
2025-04-23 14:52:55 -0700

    Testing

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include "stack.h"
+#include <array>
 
 using namespace std;
 
 int main() {
 
     Stack MyStack;
-
+    MyStack.push(10);
+    MyStack.push(20);   
+    MyStack.push(30);
 
     if (MyStack.isEmpty()){
         cout << "Stack is empty" << endl;
     } else {
         cout << "Top element:" << MyStack.peek() << endl;
     }
+
+    MyStack.pop();
+    cout << "Top element after pop:" << MyStack.peek() << endl;
+
 
     return 0;
 }
